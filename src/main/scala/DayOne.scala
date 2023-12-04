be612551021c62340be2db710f479270ee74bcfe
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-12-04 17:13:00 +0000

    I did an advent of code (part 2)

--- src/main/scala/DayOne.scala
+++ src/main/scala/DayOne.scala
@@ -9,10 +9,29 @@ def splitByLine(block: String): Array[String] = {
 /** On each line, the calibration value is
  * combining the first digit and the last digit (in that order) to form a single two-digit number.
  **/
-def extractCalibrationValues(string: String): Array[Int] = {
+def extractCalibrationValues(string: String, part2: Boolean = false): Array[Int] = {
   val lines = splitByLine(string)
-  val transform = lines.map(line => whatDisValue(line))
+  val transform = lines.map(line =>
+    if(part2) {
+      whatDisValue(part2Update(line))
+    } else
+    whatDisValue(line)
+  )
   return transform
+}
+
+def part2Update(line: String): String = {
+  //watch out for where two numbers share a letter eg. twone, nineight, eightwo
+  line
+    .replace("one", "o1e")
+    .replace("two", "t2o")
+    .replace("three", "t3e")
+    .replace("four", "4")
+    .replace("five", "5e")
+    .replace("six", "6")
+    .replace("seven", "7n")
+    .replace("eight", "e8t")
+    .replace("nine", "n9e")
 }
 
 // get first and last digits (they can be the same) and create a two-digit number
@@ -24,11 +43,20 @@ def whatDisValue(line: String): Int = {
   first.toString.concat(last.toString).toIntOption.getOrElse(0)
 }
 
-@main def dayOne() =
+@main def dayOne() = {
   println("First 'value' :P ")
   println(whatDisValue(splitByLine(input).head))
-  println(whatDisValue("bhfive4ths1811seven"))
-  println("Total items to sum up:")
-  println(extractCalibrationValues(input).length)
+  // println(whatDisValue("bhfive4ths1811seven")) //41
+  //  println("Total items to sum up:")
+  //  println(extractCalibrationValues(input).length)
   println("Expected total:")
   println(extractCalibrationValues(input).sum)
+  //  println(whatDisValue(part2Update("bhfive4ths1811seven"))) //57
+  //  println(whatDisValue(part2Update("ssfzbthree8twoneqqn"))) //31
+  println("Part 2 total:")
+  println(extractCalibrationValues(input, true).sum)
+  //53794 with direct replace one to 1
+  //53884 is too low still accounting for language...
+  //54424 too low (fixed eight problem)
+  //54473 correct, forgot sevenine haha
+}
