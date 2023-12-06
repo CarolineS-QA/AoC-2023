030456e776e64da8c2cbe3b3bf80bfe25dacfcfb
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-12-06 13:41:03 +0000

    day 2 part 2 (still lil scuffed)

--- src/main/scala/DayTwo.scala
+++ src/main/scala/DayTwo.scala
@@ -5,20 +5,36 @@ import InputUtils._
 object DayTwo {
   val input: String = Source.fromFile("src/main/resources/aoc-day2-input.txt").mkString
 
-  // game is valid if less than or equal to 12 red cubes, 13 green cubes, and 14 blue cubes
-  private def extractValidGameIndexes(string: String) = {
+  private def extractValidGameIndexes(string: String, part2: Boolean = false) = {
     val games = splitByLine(string)
     games.map(game =>
-      isGameValid(game)
+      if(part2) {
+        cubePower(game)
+      } else indexIfGameValid(game)
     )
   }
-  
-  // game is valid if less than or equal to 12 red cubes, 13 green cubes, and 14 blue cubes
-  private def isGameValid(game: String): Int = {
+
+  //yes the parsing should be it's own func :P
+  private def cubePower(game: String): Int = {
     //Game index: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     val indexedCubes = game.split(":")
-    val index = indexedCubes.head.split(" ").last
-    val allCubes = indexedCubes.last.split("[,;]")
+    val index: Int = indexedCubes.head.split(" ").last.toInt
+    val allCubes: Array[String] = indexedCubes.last.split("[,;]")
+
+    //just want max cubes of all rounds, could of reused this
+    val redMaxCount = cubeCounts(allCubes, "red").max
+    val blueMaxCount = cubeCounts(allCubes, "blue").max
+    val greenMaxCount = cubeCounts(allCubes, "green").max
+
+    redMaxCount * blueMaxCount * greenMaxCount
+  }
+
+  // game is valid if less than or equal to 12 red cubes, 13 green cubes, and 14 blue cubes
+  private def indexIfGameValid(game: String): Int = {
+    //Game index: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    val indexedCubes = game.split(":")
+    val index: Int = indexedCubes.head.split(" ").last.toInt
+    val allCubes: Array[String] = indexedCubes.last.split("[,;]")
 
     //just want max cubes of all rounds
     val redCounts = cubeCounts(allCubes, "red").filter(count => count > 12)
@@ -27,7 +43,7 @@ object DayTwo {
 
     // only return if game is below max
     if (redCounts.isEmpty && blueCounts.isEmpty && greenCounts.isEmpty) {
-      index.toInt
+      index
     } else 0
   }
 
@@ -35,9 +51,9 @@ object DayTwo {
     private def cubeCounts(cubes: Array[String], colour: String): Array[Int] = {
      cubes.filter(cube => cube.contains(colour)).map(cube => cube.replace(colour, "").trim.toInt)
     }
-  
+
   def answer(): String = {
-    s"Part 1: ${extractValidGameIndexes(input).sum} \nPart 2: ${}"
+    s"Part 1: ${extractValidGameIndexes(input).sum} \nPart 2: ${extractValidGameIndexes(input, true).sum}"
   }
 
 }
